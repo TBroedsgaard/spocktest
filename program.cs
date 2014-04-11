91087e06e4d0ec973340d7bbd418517d508f8cb2
Troels Brødsgaard <troels@broedsgaard.com>
2014-04-11 09:43:11 +0200

    change from python to c#

---
+++ program.cs
@@ -0,0 +1,9 @@
+using System;
+class HelloWorld
+{
+  static void Main()
+  {
+      System.Console.WriteLine(
+          "Hello world! This is a really long message from before you were born!");
+  }
+}
